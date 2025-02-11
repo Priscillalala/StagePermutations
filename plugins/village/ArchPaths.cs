aa81cf6006f55a69cbb9be8a444e15e26aa64afe
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2025-02-11 01:38:01 -0800

    add arch paths variation

---
+++ plugins/village/ArchPaths.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace StagePermutations.village;
+
+[RegisterPermutation("village", "Shattered Abodes", "Arch Paths Variation", description = "A set of smaller temple arches will sometimes appear")]
+public class ArchPaths : PermutationBehaviour
+{
+    public override void Apply(Scene scene, IDictionary<string, GameObject> rootObjects, SceneObjectToggleGroup toggleGroupController)
+    {
+        if (!rootObjects.TryGetValue("TOGGLE: ArchPaths", out GameObject archPathsToggle))
+        {
+            return;
+        }
+        Transform LVArc_ArchToggle = archPathsToggle.transform.Find("LVArc_ArchToggle");
+        if (!LVArc_ArchToggle)
+        {
+            return;
+        }
+        ArrayUtils.ArrayAppend(ref toggleGroupController.toggleGroups, new GameObjectToggleGroup
+        {
+            objects = [LVArc_ArchToggle.gameObject],
+            minEnabled = 0,
+            maxEnabled = 1,
+        });
+    }
+}
